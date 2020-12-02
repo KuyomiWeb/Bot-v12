96d2fd47cbd0847bb97c57d9d9cc1bd5225baf03
Kuyomi <73225184+KuyomiWeb@users.noreply.github.com>
2020-12-02 20:27:44 +0100

    Update help.js
    Permet de savoir le bot a quoi comme commande

--- cmds/help.js
+++ cmds/help.js
@@ -1,5 +1,4 @@
 const Discord = require("discord.js");
-const colours = require("../colours.json");
     
     
     module.exports.run = async (bot, message, args) => {
@@ -12,9 +11,9 @@ const colours = require("../colours.json");
             if(bot.commands.has(command)) {
                 var SHembed = new Discord.MessageEmbed()
                 .setColor(colours.blue_light)
-                .setAuthor(`" LuffyBot`, bot.user.displayAvatarURL)
+                .setAuthor(`" RandomBot`, bot.user.displayAvatarURL)
                 .setThumbnail(bot.user.displayAvatarURL)
-                .setDescription(`Le prefix du bot "ζ͜͡Luffy Bot" est : ?`)
+                .setDescription(`Le prefix du bot "RandomBot" est : ?`)
                 message.channel.send(SHembed)
                 
             }}
@@ -23,7 +22,7 @@ const colours = require("../colours.json");
     
                 let Nembed = new Discord.MessageEmbed()
                 .setTitle(`Liste des commandes :`, bot.user.displayAvatarURL)
-                .setDescription('<a:badge:762263025473421353> -  Le Préfix de **ζ͜͡Luffy Bot** est `+` Le bot contient actuellement `81 Commandes.`')
+                .setDescription('<a:badge:762263025473421353> -  Le Préfix de **ζ͜͡Random Bot** est `+` Le bot contient actuellement `81 Commandes.`')
                 .addFields(
                     { name: '<a:fleche:762262810146897960> <:badge2:762276852458455111> Administration - (11)', value: '`addrole`, `annonce`, `config`, `selogs`, `setprefix`, `clear`, `removerole`, `sondage`, `raidon`, `raidoff`, `lock`, `unlock`' },
                     { name: '<a:fleche:762262810146897960> <:555:762277725880582166> Configuration - (5)', value: '`config`, `setjoin`, `setleave`, `setlogs`, `setprefix`' },
@@ -34,14 +33,14 @@ const colours = require("../colours.json");
                     { name: '<a:fleche:762262810146897960> <:nsfw55:762288750737883156> NSFW - (8)', value: '`ass`, `boobs`, `fuck`, `gif`, `hentai`, `pussy`, `lick`, `slap`' },
                     { name: '<a:fleche:762262810146897960> <:999:762287445572059157> Divertissement - (18', value: '`about`, `spotify`, `leaderboard`, `say`, `avatar`, `botinfo`, `serverinfo`, `sondage`, `userinfo`, `channelinfo`, `embed`, `help`, `invite`, `ping`, `rank`, `stats`, `support`, `newticket`' },
                     { name: '<a:fleche:762262810146897960> <:11:762288900571922442> Owner - (7)', value: '`whitelist`, `blacklist`, `stop`, `eval`, `serverlist`, `uptime`, `reload`' },
-                    { name: '<a:fleche:762262810146897960> Lien Utile', value: '([Support](https://discord.gg/luffybot) | [Invitation](https://discord.com/api/oauth2/authorize?client_id=770669520799924274&permissions=8&scope=bot) | [Site Web](https://google.com))' },
+                    { name: '<a:fleche:762262810146897960> Lien Utile', value: '([Support](https://discord.gg/randombot) | [Invitation](https://github.com/KuyomiWeb/Bot-v12) | [Site Web](https://google.com))' },
         
         
                 )
                 .setColor("#000000")
-                .setThumbnail("https://media.discordapp.net/attachments/759130676003209216/763769425983045642/BD7NNLDHDYGL3UJAWCHHY2JKWU.jpg")
-                .setImage("https://cdn.discordapp.com/attachments/768491022199291985/770260563024412742/AOvQBfm.jpeg")
-                .setFooter("🎃・ζ͜͡Luffy Bot Modération !")
+                .setThumbnail("ta photo")
+                .setImage("mes ta photo")
+                .setFooter("🎃・ζ͜͡Random Bot Modération !")
                 .setTimestamp()
                 message.channel.send(Nembed);
             }
